4d72e39667232394ed7a94ae10f48ec7e3da21fc
Utsav Punia <jiutsav8@gmail.com>
2025-02-17 18:42:38 +1030

    """"

--- question17.sh
+++ question17.sh
@@ -5,13 +5,13 @@ cp ./dir3/bar ./dir3/bar_copy
 git add .
 git commit -m "commit1"
 
-git checkout branch branch1
+git checkout branch1
 mv ./dir1/dir2/foo ./dir1/foo
 touch newfile1
 git add .
 git commit -m "commit2"
 
-git checkout branch branch2
+git checkout branch2
 touch dir3/newfile2
 mv dir1/dir2/foo dir1/dir2/foo_modified
 git add .
