c0f4ba06a15d848c01d0220389d6f946399873fd
Utsav Punia <jiutsav8@gmail.com>
2025-02-17 19:37:33 +1030

    done

--- question18.sh
+++ question18.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 git fetch --all
-git branch -r
 git checkout main
+git pull origin main
 
 for branch in $(git branch -r | grep "ready"); do
     git merge --no-ff -m "Merged ${branch#origin/} into main" ${branch#origin/}
-    
     if [[ $(git status --porcelain) ]]; then
         git add .
         git commit -m "Resolved merge conflicts for ${branch#origin/}"
     fi
+    git push origin main
 done
 
 for branch in $(git branch -r | grep "ready"); do
@@ -23,8 +23,8 @@ for branch in $(git branch -r | grep "update"); do
         git checkout -b ${branch#origin/} origin/${branch#origin/}
     else
         git checkout ${branch#origin/}
+        git pull origin ${branch#origin/}
     fi
-
     git merge main -m "Updated ${branch#origin/} with latest changes from main"
     git push origin ${branch#origin/}
 done
